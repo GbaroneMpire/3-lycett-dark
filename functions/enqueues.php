894322794e3338453e2bca02973c4bb9fdc31fca
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-08-06 17:37:49 +0200

    update

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -7,8 +7,8 @@
 function b4st_enqueues() {
 
 	// Styles
-	// wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css', false, '4.6.2', null);
-	// wp_enqueue_style('bootstrap');
+	wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css', false, '4.6.2', null);
+	wp_enqueue_style('bootstrap');
 
 	wp_register_style('fontawesome5', 'https://use.fontawesome.com/releases/v5.8.1/css/all.css', false, '5.8.1', null);
 	wp_enqueue_style('fontawesome5');
