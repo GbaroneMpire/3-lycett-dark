473078af7668a010ad2b105c93eec8e95bf85349
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-08-06 10:33:24 +0200

    update

--- template-parts/intro.php
+++ template-parts/intro.php
@@ -1,7 +1,7 @@
 <div id="lct-intro">
 
-  <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/theme/css/intro.css ?>">
-  
+  <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/theme/css/intro.css">
+
   <div class="inner">
     <?= get_template_part('template-parts/logo', 'square'); ?>
   </div>
