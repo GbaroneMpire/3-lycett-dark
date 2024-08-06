2b1dc32049e35d9aeb52c52712ce35c0f66c0e46
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-08-06 19:39:32 +0200

    test

--- template-parts/gallery-section.php
+++ template-parts/gallery-section.php
@@ -8,23 +8,28 @@
 
 <section id="lct-gallery-section" class="lct-gallery-section lct-section">
 
-  <section class="container lct-smaller lct-title-section">
-    <div class="row">
-      <div class="col text-center">
-        <h2><?= $fields['heading'] ?></h2>
-      </div>
-    </div>
-  </section>
+  <div class="lct-inner">
 
-  <section class="gallery-section text-white p-0">
-    <div class="row no-gutters">
-      <div class="col">
-      <?php get_template_part('/template-parts/gallery', null, array(
-        'fields' => $fields['lct_gallery']
-      )); ?>
-      
+    <section class="container lct-smaller lct-title-section">
+      <div class="row">
+        <div class="col text-center">
+          <h2><?= $fields['heading'] ?></h2>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  
+    <section class="gallery-section text-white p-0">
+      <div class="row no-gutters">
+        <div class="col">
+        <?php get_template_part('/template-parts/gallery', null, array(
+          'fields' => $fields['lct_gallery']
+        )); ?>
+        
+        </div>
+      </div>
+    </section>
+
+  </div>
+
 
 </section>
