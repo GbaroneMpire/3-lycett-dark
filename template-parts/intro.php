694b6124661e0114c2832601d067916274a1c7c9
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-08-06 10:37:48 +0200

    update

--- template-parts/intro.php
+++ template-parts/intro.php
@@ -2,7 +2,7 @@
 
   <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/theme/css/intro.css">
 
-  <div class="inner d-flex flex-column align-items-center justify-content-center">
+  <div class="inner d-flex flex-column align-items-center justify-content-center w-100 h-100">
     <?= get_template_part('template-parts/logo', 'square'); ?>
   </div>
 </div>
