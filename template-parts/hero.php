7359323e4b41718d8087d0dd126ff0d9957f61da
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-08-06 11:54:33 +0200

    test

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -34,12 +34,12 @@
                   if (is_string($video_bg)):
         
                     $vid_string_length = strlen($video_bg);
-                    $str_to_insert = ' muted';
+                    $str_to_insert = ' muted ';
                     $newstr = substr_replace($video_bg, $str_to_insert, $vid_string_length - 2, 0);
 
                     echo $newstr;
-                    
-                    echo do_shortcode($video_bg);
+
+                    echo do_shortcode($newstr);
                   else:
                     echo $video_bg;
                   endif;
