7af18ee7d7f63b8122595d33c30e0b28042b4481
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-08-08 20:28:22 +0200

    test

--- footer.php
+++ footer.php
@@ -37,12 +37,12 @@
 
   foreach ($layout_sections as $section):
 
-    $hero = $section['hero']['lct_hero'];
+    $hero = $section['hero'];
 
     if ($hero): 
 
       lct_test($hero);
-      
+
     endif;
 
 
