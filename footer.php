da6d211fcaed4e1b181ed9b8d3b056951876bb78
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-08-12 11:28:36 +0200

    update

--- footer.php
+++ footer.php
@@ -15,15 +15,15 @@
     <div class="row pt-3">
 
         <div class="col-lg">
-          <p class="text-center text-sm-left">&copy; <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a> <?php echo date('Y'); ?></p>
+          <p class="text-center text-lg-left">&copy; <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a> <?php echo date('Y'); ?></p>
         </div>
 
         <div class="col-lg">
-          <p class="text-center text-sm-left">Contact <strong>3lycett@gmail.com</strong> for further questions or general inquiries.</p>
+          <p class="text-center">Contact <strong>3lycett@gmail.com</strong> for further questions or general inquiries.</p>
         </div>
 
         <div class="col-lg">
-          <p class="text-center text-sm-right">Designed & Developed by <a href="https://mpirecreative.com"><span class="mpire-m">M</span>pire Creative</a></p>
+          <p class="text-center text-lg-right">Designed & Developed by <a href="https://mpirecreative.com"><span class="mpire-m">M</span>pire Creative</a></p>
         </div>
         
     </div>
