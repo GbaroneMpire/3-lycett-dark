103c4987d5f5b011a872d30e3c7886b92320f5c8
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-08-20 19:43:45 +0200

    custom styels

--- template-parts/modals.php
+++ template-parts/modals.php
@@ -4,29 +4,35 @@
 
   foreach($modals as $modal): 
 
-      $modal_title = $modal['title']
+      $modal_title = $modal['title'];
+      $custom_styles = $modal['custom_styles'];
+
     ?>
 
-          <div id="<?= $modal['id']; ?>" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="<?= $modal['id']; ?>">
-              <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
-                <div class="modal-content px-5 py-3">
+      <?php if ($custom_styles): ?>
+        <?= $custom_styles; ?>
+      <?php endif; ?>
 
-                  <div class="modal-header justify-content-center border-0">
-                    <?php if($modal_title): ?> 
-                      <h4 class="modal-title"><?= $modal_title; ?></h4>
-                    <?php endif; ?>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true"><i class="fa-sharp fa-thin fa-xmark"></i></span>
-                    </button>
-                  </div>
+      <div id="<?= $modal['id']; ?>" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="<?= $modal['id']; ?>">
+          <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+            <div class="modal-content px-5 py-3">
 
-                  <div class="modal-body">
-                    <?= $modal['content']; ?>
-                  </div>
-
-                </div>
+              <div class="modal-header justify-content-center border-0">
+                <?php if($modal_title): ?> 
+                  <h4 class="modal-title"><?= $modal_title; ?></h4>
+                <?php endif; ?>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true"><i class="fa-sharp fa-thin fa-xmark"></i></span>
+                </button>
               </div>
-            </div> <?php 
+
+              <div class="modal-body">
+                <?= $modal['content']; ?>
+              </div>
+
+            </div>
+          </div>
+        </div> <?php 
   
   endforeach;
 
